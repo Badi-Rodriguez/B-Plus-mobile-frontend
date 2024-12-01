e4d763e527becf6e2fd7953b570b6dc38dd4f8fe
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-12-01 17:52:16 -0500

    MEQUIEROMATAR

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import 'react-native-gesture-handler'; // Import this at the very top
+import 'react-native-gesture-handler';  // Import this at the very top
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStaticNavigation, NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -25,7 +25,7 @@ function MainTabs() {
                 tabBarInactiveTintColor: 'gray',  // Customize inactive tab icon color
                 tabBarStyle: { backgroundColor: '#fff' },  // Customize tab bar style
             }}
-         id={}>
+        >
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Modules" component={ModulesScreen} />
             <Tab.Screen name="Habits" component={HabitTrackerScreen} />
@@ -38,14 +38,15 @@ function MainTabs() {
 // Main App Component (Stack Navigator)
 export default function App() {
     return (
-
-            <Stack.Navigator initialRouteName="Main" id={}>
+        <NavigationContainer>
+            <Stack.Navigator>
                 <Stack.Screen
                     name="Main"
                     component={MainTabs}
                     options={{ headerShown: false }} // Hide header for the main screen
                 />
             </Stack.Navigator>
+        </NavigationContainer>
     );
 }
 
